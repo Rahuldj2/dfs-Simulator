489ed3ed7a4c4ec689bd11d428fc9115d7d76c65
Rahuldj <rj712@snu.edu.in>
2023-08-30 22:20:29 +0530

    code-formatted

--- src/Code.js
+++ src/Code.js
@@ -1,62 +1,32 @@
+import React from 'react';
 
-
-let code=`
-  void dfs(ArrayList<ArrayList<Integer>> graph,int curVertex)\n
-  {\n
-    \tArrayList<Integer> adjList = graph.get(curVertex);\n
-    \tvisited[curVertex]=true;\n
-    \tfor (int i=0;i<adjList.size();i++)\n
-    \t{
-        \tint neighbour=adjList.get(i);\n
-        \tif (visited[neighbour]==false)\n
-        \t{
-            \tdfs(graph,neighbour);\n
-        \t}\n
-    \t}\n
-  \t}\n
-  `
-
-
-const Code=()=>
+const Code = () => {
+  let code = `
+void dfs(ArrayList<ArrayList<Integer>> graph, int curVertex)
 {
-  
-let codeList=[`void dfs(ArrayList<ArrayList<Integer>> graph,int curVertex)`,
-'{\n',
-`ArrayList<Integer> adjList = graph.get(curVertex);\n`,
-`    visited[curVertex]=true;\n`,
-`    for (int i=0;i<adjList.size();i++)\n`,
-`    {`,
-`        int neighbour=adjList.get(i);\n`,
-`        if (visited[neighbour]==false)\n`,
-`        {`,
-`            dfs(graph,neighbour);\n`,
-`        }\n`,
-`    }\n`,
-`  }\n`,
-
-]
-
-let display=[]
-
-
-for (let i=0;i<codeList.length;i++)
-{
-  display[i]=<code>{codeList[i]}<br></br></code>
+    ArrayList<Integer> adjList = graph.get(curVertex);
+    visited[curVertex] = true;
+    for (int i = 0; i < adjList.size(); i++)
+    {
+        int neighbour = adjList.get(i);
+        if (!visited[neighbour])
+        {
+            dfs(graph, neighbour);
+        }
+    }
 }
-    return (
-        
-        <div className='code'>
-        <div>
-          <h1>
-            CODE IN JAVA
-          </h1>
-          <code>
-          {display}
-          </code>
-          
-        </div>
+`;
+
+  return (
+    <div className='code'>
+      <div>
+        <h1>CODE IN JAVA</h1>
+        <pre>
+          <code>{code}</code>
+        </pre>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Code;
